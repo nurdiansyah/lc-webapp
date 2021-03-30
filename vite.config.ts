f647032cb3dcd8d07366f13ed2e6b561620e4552
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-03-30 23:10:06 +0700

    setting

---
+++ vite.config.ts
@@ -0,0 +1,57 @@
+import { resolve } from "path";
+import svelte from "@sveltejs/vite-plugin-svelte";
+// import windiCSS from "vite-plugin-windicss";
+import { minify } from "html-minifier";
+import { mdsvex } from "mdsvex";
+import slug from "remark-slug";
+import svelteConfig from "./svelte.config";
+
+// const { defineConfig } = require("vite");
+import { defineConfig } from "vite";
+const indexReplace = () => {
+  return {
+    name: "html-transform",
+    transformIndexHtml(html) {
+      return minify(html, {
+        collapseWhitespace: true
+      });
+    }
+  };
+};
+
+export default defineConfig(({ command, mode }) => {
+  const isProduction = mode === "production";
+  return {
+    build: {
+      polyfillDynamicImport: false,
+      cssCodeSplit: false,
+      minify: isProduction
+    },
+
+    optimizeDeps: {
+      exclude: ["@roxi/routify"]
+    },
+    resolve: {
+      dedupe: ["@roxi/routify"],
+      alias: {
+        // svelte: resolve(__dirname, "node_modules/svelte"),
+        "@": resolve(__dirname, "./src")
+      }
+    },
+    plugins: [
+      // windiCSS({
+      //   //@ts-ignore
+      //   verbose: true,
+      //   silent: false,
+      //   debug: true,
+      //   config: "tailwind.config.js", // tailwind config file path (optional)
+      //   compile: false, // false: interpretation mode; true: compilation mode
+      //   prefix: "windi-", // set compilation mode style prefix
+      //   globalPreflight: true, // set preflight style is global or scoped
+      //   globalUtility: true // set utility style is global or scoped
+      // }),
+      svelte(svelteConfig),
+      indexReplace()
+    ]
+  };
+});
