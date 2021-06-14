905a8db4ab42d4996dc246e4a91d64d852e13a43
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-06-14 23:33:27 +0700

    chore: break rollup to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { resolve } from "path";
-import svelte from "@sveltejs/vite-plugin-svelte";
-// import windiCSS from "vite-plugin-windicss";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { replaceCodePlugin } from "vite-plugin-replace";
 import { minify } from "html-minifier";
-import { mdsvex } from "mdsvex";
-import slug from "remark-slug";
-import svelteConfig from "./svelte.config";
+import { preprocess } from "./svelte.config";
 
 // const { defineConfig } = require("vite");
 import { defineConfig } from "vite";
@@ -22,6 +20,9 @@ const indexReplace = () => {
 export default defineConfig(({ command, mode }) => {
   const isProduction = mode === "production";
   return {
+    server: {
+      port: 5000
+    },
     build: {
       polyfillDynamicImport: false,
       cssCodeSplit: false,
@@ -34,11 +35,34 @@ export default defineConfig(({ command, mode }) => {
     resolve: {
       dedupe: ["@roxi/routify"],
       alias: {
-        // svelte: resolve(__dirname, "node_modules/svelte"),
-        "@": resolve(__dirname, "./src")
+        svelte: resolve(__dirname, "node_modules/svelte"),
+        "__@comps": resolve(__dirname, "./src/components"),
+        "__@modules": resolve(__dirname, "./src/modules"),
+        "__@stores": resolve(__dirname, "./src/stores"),
+        "__@root": resolve(__dirname, "./src")
       }
     },
     plugins: [
+      replaceCodePlugin({
+        replacements: [
+          {
+            from: "process.env.DBX_ENV_GRAPHQL_URL",
+            to: `"${process.env.DBX_ENV_GRAPHQL_URL}"`
+          },
+          {
+            from: "process.env.DBX_ENV_GRAPHQL_WS",
+            to: `"${process.env.DBX_ENV_GRAPHQL_WS}"`
+          },
+          {
+            from: "process.env.DBX_ENV_API_URL",
+            to: `"${process.env.DBX_ENV_API_URL}"`
+          },
+          {
+            from: "process.env.NODE_ENV",
+            to: `"${process.env.NODE_ENV || "development"}"`
+          }
+        ]
+      }),
       // windiCSS({
       //   //@ts-ignore
       //   verbose: true,
@@ -50,7 +74,12 @@ export default defineConfig(({ command, mode }) => {
       //   globalPreflight: true, // set preflight style is global or scoped
       //   globalUtility: true // set utility style is global or scoped
       // }),
-      svelte(svelteConfig),
+      svelte({
+        hot: !isProduction,
+        emitCss: true,
+        extensions: [".svelte"],
+        preprocess
+      }),
       indexReplace()
     ]
   };
